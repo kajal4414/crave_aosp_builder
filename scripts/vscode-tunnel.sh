f040bc018561fcf51f661845e983824f47902643
kajal4414 <81718060+kajal4414@users.noreply.github.com>
2024-06-25 01:42:54 +0530

    Revert last 3 commits
    Revert "Test crDroid 13.0"
    
    This reverts commit 58c3bc1068bf2f45033f18b69fddb9a4a977b33d.
    
    Signed-off-by: kajal4414 <81718060+kajal4414@users.noreply.github.com>
    
    Revert "Adapt for spes"
    
    This reverts commit 232eca1cfe185fbf37ea383da211193734614c5f.
    
    Signed-off-by: kajal4414 <81718060+kajal4414@users.noreply.github.com>
    
    Revert "Format all docs"
    
    This reverts commit f97052a2d9f9f37865910278705abc3422686fff.
    
    Signed-off-by: kajal4414 <81718060+kajal4414@users.noreply.github.com>

--- scripts/vscode-tunnel.sh
+++ scripts/vscode-tunnel.sh
@@ -7,16 +7,16 @@ if [ -d "code-tunnel" ]; then
   echo "skipping download!"
 else
   echo "Installing Runner!" #Create folder and download zip
-  echo "Downloading Zip"
+  echo "Downloading Zip"  
   curl -Lk 'https://code.visualstudio.com/sha/download?build=stable&os=cli-alpine-x64' --output vscode_cli_alpine_x64_cli.tar.gz
   echo "Extracting Zip"
-  mkdir -p code-tunnel
-  tar -xvf vscode_cli_alpine_x64_cli.tar.gz -C code-tunnel
+    mkdir -p code-tunnel
+    tar -xvf vscode_cli_alpine_x64_cli.tar.gz -C code-tunnel
   echo "Removing Leftovers"
-  rm -rf vscode_cli_alpine_x64_cli.tar.gz # Extract package and remove leftovers
+    rm -rf vscode_cli_alpine_x64_cli.tar.gz # Extract package and remove leftovers
 fi
 
 tmux kill-session -t codetunnel || true
 tmux new-session -d -s codetunnel
-tmux send-keys -t codetunnel './code-tunnel/code tunnel --accept-server-license-terms' Enter
+tmux send-keys -t codetunnel './code-tunnel/code tunnel --accept-server-license-terms' Enter 
 echo "Runner Started"
